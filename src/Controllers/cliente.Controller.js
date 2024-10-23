ced278411f0c4a7ce75f608833f1b36ba17e7eba
Mauricio Valentín Barón Salinas <ba488744@uaeh.edu.mx>
2024-10-22 21:14:03 -0600

    Se cambia el retorno de eliminarCliente; ahora manda un mensaje con el nombre del cliente eliminado.

--- src/Controllers/cliente.Controller.js
+++ src/Controllers/cliente.Controller.js
@@ -100,7 +100,9 @@ export const eliminarCliente = async (req, res) => {
       // Si no tenemos cliente
       return res.status(404).json({ message: "Cliente no encontrado" });
     } // de lo contrario
-    res.json(cliente); // se manda el cliente eliminado
+    res.json({
+      message: `El cliente ${cliente.nombre} ${cliente.apellidoPaterno} ${cliente.apellidoMaterno} ha sido eliminado.`,
+    }); // se manda el cliente eliminado
   } catch (error) {
     res.status(500).json({ message: error.message }); // mandamos el error
   }
